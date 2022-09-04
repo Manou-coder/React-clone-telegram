fb5bd1e425081005cb839e278bda7d3888606217
Manou <manouhaouzi@gmail.com>
2022-09-04 07:10:20 +0300

    fix size of timeBadge for the smartphone

--- src/components/chat/TimeBadge.js
+++ src/components/chat/TimeBadge.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class TimeBadge extends Component {
   render() {
     return (
-      <div className='row w-100 justify-content-center mb-3'>
-        <div className='col-2 py-1 bg-white rounded text-center opacity-75'>
-            <span>{new Date().toLocaleDateString()}</span>
+      <div className="row w-100 justify-content-center mb-3">
+        <div className="col-4 col-md-3 col-lg-2 py-1 bg-white rounded text-center opacity-75">
+          <span>{new Date().toLocaleDateString()}</span>
         </div>
       </div>
     )
